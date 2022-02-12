6ac6581b866d501c94864694523c96cc068fc297
Mureed Hussain <imhussain0086@gmail.com>
2022-02-12 22:10:44 +0500

    First Update
    First Update

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomExp from "./components/IncomExp";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+
+import { GlobalProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container"></div>
+      <Balance />
+      <IncomExp />
+      <TransactionList />
+      <AddTransaction />
+    </GlobalProvider>
+  );
+}
+
+export default App;
